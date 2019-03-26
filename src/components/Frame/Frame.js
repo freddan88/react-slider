f150ca888e5a013562b1b0e3162033be2b49b3d5
Fredrik Leemann <fredrik@leemann.se>
2019-03-26 15:30:10 +0100

    Updated colors on frame

--- src/components/Frame/Frame.js
+++ src/components/Frame/Frame.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Frame = styled.div`
   width: 700px;
   height: 466px;
-  border: 50px solid gold;
+  border: 50px solid rgba(226,192,180,0.8);
   overflow: hidden;
   position: relative;
 `;
